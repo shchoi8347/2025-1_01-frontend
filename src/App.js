d03b5b024f1bbb437ce5792becbfc59d75f53483
shchoi8347 <csh@duksung.ac.kr>
2025-04-03 11:47:13 +0900

    수업완료코드: 20250403(목) - 01반

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import Todo from './Todo';
+import React, {useState} from "react";
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: "0",
+      title: "Hello World 1",
+      done: true
+    }, {
+      id: "1",
+      title: "Hello World 2",
+      doen: false
+    }
+  ]);
+
+  let todoItems =
+    items.length > 0 && items.map((item) => <Todo item={item} key={item.id} />);
+    
   return (
     <div>
-      <Todo />
-      <Todo />
+      {todoItems}
     </div>
   );
 }
