51120da5e0019996afb8d4c2a6550a34b04d8f6d
shchoi8347 <csh@duksung.ac.kr>
2025-05-06 10:19:48 +0900

    수업완료코드: 20250506(화) 01반반

---
+++ src/Login.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Container, Grid, Typography, TextField, Button} from "@mui/material";
+import { signin } from "./service/ApiService";
+
+const Login = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        const username = data.get("username");
+        const password = data.get("password");
+
+        signin({username:username, password:password});
+        //signin({username:"hello@world.com", password:"12345"});
+    };
+
+    return(
+        <Container component="main" maxWidth="xs" style={{marginTop: "8%"}}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Typography component="h1" variant="h5">
+                        로그인
+                    </Typography>
+                </Grid>
+            </Grid>
+            <form noValidate onSubmit={handleSubmit}>
+                {" "}
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="username"
+                            label="아이디"
+                            name="username"
+                            autoComplete="username"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="password"
+                            label="패스워드"
+                            type="password"
+                            name="password"
+                            autoComplete="current-password"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button type="submit" fullWidth variant="contained" color="primary">
+                            로그인
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </Container>
+    );
+}
+
+export default Login;
--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -13,11 +13,25 @@ export function call(api, method, request) {
     }
 
     return fetch(options.url, options).then((response) => {
+        console.log("csh: response status : " + response.status)
         if( response.status === 200 ) {
             return response.json();
+        } else if( response.status === 403) {
+            window.location.href = "/login";
+        } else {
+            Promise.reject(response);
+            throw Error(response);
         }
     }).catch((error) => {
         console.log("http error");
         console.log(error);
     });
+}
+
+export function signin(userDTO) {
+    return call("/auth/signin", "POST", userDTO) 
+        .then((response) => {
+            console.log("response: ", response);
+            alert("로그인 토큰: " + response.token);
+        });
 }
