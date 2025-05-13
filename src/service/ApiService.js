a1feca76149a7157f5b13924e376499468363c31
shchoi8347 <csh@duksung.ac.kr>
2025-05-13 10:14:48 +0900

    수업완료코드: 01반 20250513(화)

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -53,3 +53,8 @@ export function signout() {
     localStorage.setItem("ACCESS_TOKEN", null);
     window.location.href = "/login"
 }
+
+// 계정 생성 함수
+export function signup(userDTO) {
+    return call("/auth/signup", "POST", userDTO);
+}
